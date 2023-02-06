072cc039e9521a532070f4a659e2a4283c502109
Mohammed Mubashir <mohammedmubashir204@gmail.com>
2023-02-06 12:46:23 +0530

    Initial Commit

---
+++ photonix/photonixApp.swift
@@ -0,0 +1,17 @@
+//
+//  photonixApp.swift
+//  photonix
+//
+//  Created by Mohammed Mubashir on 06/02/23.
+//
+
+import SwiftUI
+
+@main
+struct photonixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
