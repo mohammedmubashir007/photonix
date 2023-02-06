ba8931627d625f31003c421f4d8751441f581307
Mohammed Mubashir <mohammedmubashir204@gmail.com>
2023-02-06 14:41:26 +0530

    - use lazy grid to show rectangles - use geometry reader to frame them according to size

--- photonix/ContentView.swift
+++ photonix/ContentView.swift
@@ -8,15 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let columns = [GridItem(.flexible()),GridItem(.flexible())]
+    
+    init(){
+        (UIApplication.shared.connectedScenes.first as? UIWindowScene)?.windows.first!.overrideUserInterfaceStyle = .dark
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        GeometryReader{geo in
+            
+            ScrollView {
+                
+                LazyVGrid(columns: columns) {
+                    ForEach(1...10,id: \.self) {i in
+                        Color.orange.frame(width: geo.size.width/2, height: geo.size.height/2)
+                    }
+                }
+                
+         
+        HStack {
+            VStack {
+                Text("50 Soap bubbles").foregroundColor(.white).font(.title2)
+                
+                Text("See what's coming in next update").foregroundColor(.white)
+                Link("Subscribe Me on Youtube :)", destination: URL(string: "https://www.youtube.com/@mohammedmubashir/videos")!).foregroundColor(.blue)
+                
+            }
         }
-        .padding()
-        .navigationBarBackButtonHidden()
+        
+    }
+        }.navigationBarBackButtonHidden()
+            
+        
     }
 }
 
