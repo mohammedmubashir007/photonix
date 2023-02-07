577858d84bb2121eff0adfb5de289177a24789f2
Mohammed Mubashir <mohammedmubashir204@gmail.com>
2023-02-07 22:07:12 +0530

    - use geometry reader and make rectangles and show other content - make sheet which shows about details - make custom view for blur background

--- photonix/ContentView.swift
+++ photonix/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
     
     let columns = [GridItem(.flexible()),GridItem(.flexible())]
+    @State var openMenu:Bool = false
     
     init(){
         (UIApplication.shared.connectedScenes.first as? UIWindowScene)?.windows.first!.overrideUserInterfaceStyle = .dark
@@ -26,23 +27,70 @@ struct ContentView: View {
                     }
                 }
                 
-         
-        HStack {
-            VStack {
-                Text("50 Soap bubbles").foregroundColor(.white).font(.title2)
-                
-                Text("See what's coming in next update").foregroundColor(.white)
-                Link("Subscribe Me on Youtube :)", destination: URL(string: "https://www.youtube.com/@mohammedmubashir/videos")!).foregroundColor(.blue)
+                HStack {
+                    VStack {
+                        Text("50 Soap bubbles").foregroundColor(.white).font(.title2)
+                        
+                        Text("See what's coming in next update").foregroundColor(.white)
+                        Link("Subscribe Me on Youtube :)", destination: URL(string: "https://www.youtube.com/@mohammedmubashir/videos")!).foregroundColor(.blue)
+                        
+                    }
+                }
                 
             }
+        }.navigationBarItems(trailing: Button(action:{
+            openMenu = true
+            print("triggered")
+        },label: {
+            Image(systemName: "ellipsis").font(.title)
+        }))
+        .navigationBarBackButtonHidden()
+        .navigationBarTitleDisplayMode(.inline)
+        .sheet(isPresented: $openMenu, content:{
+            ZStack{
+                
+                Color.white.opacity(0).edgesIgnoringSafeArea(.all)
+                VStack(spacing: 30){
+                    Text("").padding(30)
+                    Button("About", action:{
+                        openMenu.toggle()
+                    }).font(.title)
+                    Button("App Icon", action:{
+                        openMenu.toggle()
+                    }).font(.title)
+                    Button("License Agreement", action:{
+                        openMenu.toggle()
+                    }).font(.title)
+                    Button("Privacy Policy", action:{
+                        openMenu.toggle()
+                    }).font(.title)
+                    Spacer()
+                    Text("App Version 1.0").font(.subheadline).padding(20)
+                }
+            }.background(BackgroundBlurView())
         }
-        
+        )
     }
-        }.navigationBarBackButtonHidden()
-            
-        
-    }
+    
 }
+
+
+struct BackgroundBlurView:UIViewRepresentable{
+    func makeUIView(context:Context) -> some UIView {
+        let view = UIVisualEffectView(effect: UIBlurEffect(style: .systemUltraThinMaterial))
+        
+        DispatchQueue.main.async {
+            view.superview?.superview?.backgroundColor = .clear
+        }
+        return view
+    }
+    
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+        
+    }
+    
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
